476b31698f76eee9a6b08d7766304341448b0726
leticialbuk <leticia.albuk@gmail.com>
2023-07-11 21:51:35 -0300

    mapeamendo de chave primária e identity

---
+++ Data/Mapping/CategoryMap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraticaEF_2.Data.Mapping
+{
+    internal class CategoryMap
+    {
+    }
+}
