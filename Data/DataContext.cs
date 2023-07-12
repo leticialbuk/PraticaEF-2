b1ad80a2f01b43c196c3df4d6666e0762717995f
leticialbuk <leticia.albuk@gmail.com>
2023-07-11 21:24:34 -0300

    organizando classes

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraticaEF_2.Data
+{
+    internal class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=Blog;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
